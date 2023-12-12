7b5a9231b1fbf9c75df7ab86dd3f6c8c579aa331
Ciuca.20107 <Ciuca.20107@LABI2-13PC07>
2023-12-12 11:57:21 +0100

    Aggiungere i file di progetto.

---
+++ automobile/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace automobile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ON/OF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "FRENO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(80, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ACCELERA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(80, 182);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 60);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "INNESTO";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(183, 182);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(81, 60);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "SCALARE";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(326, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(326, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ automobile/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace automobile
+{
+    public partial class Form1 : Form
+    {
+        macchina macchina = new macchina();
+        bool a = false;
+        int b=0,c=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "SPENTO";
+            textBox2.Text = "0 km/h";
+            textBox3.Text = "1°";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            b = macchina.acc(b);
+            textBox2.Text = Convert.ToString(b)+" km/h";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a=macchina.onOf(a);
+            if (a == true)
+            {
+                textBox1.Text = "ACCESO";
+            }
+            else { textBox1.Text = "SPENTO"; }
+        }
+
+
+    }
+    class macchina
+    {
+        protected bool a { get; set; }
+        protected int b { get; set; }
+        protected int c { get; set; }
+
+
+        public bool onOf(bool a)
+        {
+
+            if (a == false)
+            {
+                a = true;
+            }
+            else
+            {
+                a = false;
+            }
+            return a;
+        }
+
+        public int acc(int b)
+        {
+            b += 1;
+            return b;
+        }
+    }
+}
